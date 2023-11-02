0267e5d13e151c1cd27207319deb89dfcd4ecc9b
Tim Dawson <tidawson@redhat.com>
2023-11-02 14:08:28 +1300

    output tsv

--- scan.sh
+++ scan.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -euf -o pipefail
 
-kubectl delete configmap services
-kubectl delete -f scanjob.yaml
+
+kubectl delete configmap services || true
+kubectl delete -f scanjob.yaml || true
 
 kubectl get svc -A -o json |\
 jq -r '.items[] | select(.metadata.namespace | startswith("openshift-"))  |  select(.spec.ports!=null) | . as $svc | .spec.ports[] | select(.protocol=="TCP") | .port | tostring |  $svc.metadata.name + "." + $svc.metadata.namespace + ".svc.cluster.local" + ":" + . ' |\
@@ -15,3 +17,10 @@ kubectl exec -n default -it job/testssl -- testssl.sh  --quiet --file /input/ser
 
 mkdir -p output
 kubectl exec job/testssl -- tar -cf - -C /tmp/output . | tar -xf - -C output
+
+kubectl delete configmap services
+kubectl delete -f scanjob.yaml
+
+#find output -type f | xargs -n1 jq '.[] | select(.finding | endswith("available"))' >> available.json
+
+find output -type f | xargs -n1 jq '.[] | select(.finding | endswith("available"))' | jq -r '[ ( .ip+":"+.port ), .finding ] | @tsv' > available.tsv
